d04be589d5c91922d619a4ca50dfcb44bb84f82e
RugerMc <550279039@qq.com>
2023-01-13 11:09:07 +0800

    fix: 根据IP检索广播地址检查设备

--- app/src/main/java/top/caffreyfans/irbaby/firmware_api/UdpApi.java
+++ app/src/main/java/top/caffreyfans/irbaby/firmware_api/UdpApi.java
@@ -3,9 +3,16 @@ package top.caffreyfans.irbaby.firmware_api;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.net.Inet4Address;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import top.caffreyfans.irbaby.helper.UdpSendThread;
 import top.caffreyfans.irbaby.model.ApplianceInfo;
@@ -39,13 +46,33 @@ public class UdpApi extends Api {
                 (mIP >> 24 & 0xff));
     }
 
+    public String fetchBroadcastAddressByIP(String ip) {
+        try {
+            Enumeration networkInterfaceEnumeration = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaceEnumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = (NetworkInterface) networkInterfaceEnumeration.nextElement();
+                if (!networkInterface.isUp()) {
+                    continue;
+                }
+                for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+                    if (interfaceAddress.getAddress() instanceof Inet4Address
+                            && interfaceAddress.getAddress().getHostAddress().equals(ip)
+                    ) {
+                        return interfaceAddress.getBroadcast().getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(mContext, "没有找到合适的广播地址", Toast.LENGTH_SHORT).show();
+        return "0.0.0.0";
+    }
+
     public void discoverDevice() {
         JSONObject msg = new JSONObject();
         JSONObject params = new JSONObject();
-        String broadcast = String.format(
-                "%d.%d.%d.%d", (mIP & 0xff),
-                (mIP >> 8 & 0xff), (mIP >> 16 & 0xff),
-                (0xff));
+        String broadcast = fetchBroadcastAddressByIP(mLocalIP);
         try {
             msg.put("cmd", "query");
             params.put("ip", mLocalIP);
